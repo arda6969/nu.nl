25664b9c8049131019a50adc20e4b692d72f436e
KoenVDG <127857512+KoenVDG@users.noreply.github.com>
2023-06-06 13:33:02 +0200

    Add files via upload

---
+++ nu.nl prject/homepagina.php
@@ -0,0 +1,79 @@
+<php?>
+
+</php>
+
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>homepagina</title>
+    <link rel="stylesheet" href="homepagina.css">
+</head>
+<body>
+<a href="/school/php/nu.nl prject/homepagina.php" class="top-logo">
+    <img src="Nu-nl-logo.png" alt="Voorpagina">
+</a>
+
+<ul class="main-menu">
+                    <li class="nav_item  item_default">
+    <a href="/net-binnen" title="Net binnen">
+        Net binnen
+    </a>
+</li>
+                    <li class="nav_item  item_default">
+    <a href="/algemeen" title="Algemeen">
+        Algemeen
+    </a>
+</li>
+                    <li class="nav_item  item_nuzakelijk">
+    <a href="/economie" title="Economie">
+        Economie
+    </a>
+</li>
+                    <li class="nav_item  item_nusport">
+    <a href="/sport" title="Sport">
+        Sport
+    </a>
+</li>
+                    <li class="nav_item  item_nuuit">
+    <a href="/entertainment" title="Media en Cultuur">
+        Media en Cultuur
+    </a>
+</li>
+                    <li class="nav_item  item_nuentertainment">
+    <a href="/achterklap" title="Achterklap">
+        Achterklap
+    </a>
+</li>
+                    <li class="nav_item  item_default">
+    <a href="/nushop-adverteerder" title="Shop">
+        Shop
+    </a>
+</li>
+<li class="nav_item mobile-menu-only">
+    <a title="voorpagina" href="/" class="current">
+        Voorpagina
+    </a>
+</li>
+<li class="nav_item mobile-menu-only">
+    <a title="Meest recent" href="/net-binnen">
+        Net binnen
+    </a>
+</li>
+<li class="nav_item mobile-menu-only">
+    <a title="Meest gelezen" href="/meest-gelezen">
+        Populair
+    </a>
+</li>
+                   <li class="nav_item nav_more both-mobile-desktop">
+                        <a title="meer navigatie opties" href="#">
+                            <span class="more">Meer</span>
+                            <span class="less">Minder</span>
+                        </a>
+                    </li>
+            </ul>
+            
+</body>
+</html>
