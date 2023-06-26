8aad77cfcb7071a7c73f7494aa1120988006e206
KoenVDG <127857512+KoenVDG@users.noreply.github.com>
2023-06-26 14:55:52 +0200

    Add files via upload

--- footernu.nl/footer.php
+++ footernu.nl/footer.php
@@ -6,7 +6,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Footer</title>
-        <link rel="stylesheet" href="footer.css">
+        <link rel="stylesheet" href="../footernu.nl/footer3.css">
         <script>
             function myFunction() {
                 var element = document.body;
@@ -15,7 +15,7 @@
         </script>
     </head>
 
-    <body>
+    <body class="bodie">
         <div class="lichaam">
             <div class="dropdown-menu navigation">
                 <ul class="site_overview">
@@ -23,184 +23,184 @@
                         <a href="https://www.nu.nl/">Voorpagina</a>
                         <ul class="nav_submenu">
                             <li class="nav_subitem">
-                                <a href="https://www.nu.nl/net-binnen">Net binnen</a>
+                                <a class="patatje" href="https://www.nu.nl/net-binnen">Net binnen</a>
                             </li>
                             <li class="nav_subitem">
-                                <a href="https://www.nu.nl/meest-gelezen">Meest gelezen</a>
+                                <a class="patatje" href="https://www.nu.nl/meest-gelezen">Meest gelezen</a>
                             </li>
                             <li class="nav_subitem">
-                                <a href="https://www.nu.nl/spanningen-oekraine">Oorlog in Oekraïne</a>
+                                <a class="patatje" href="https://www.nu.nl/spanningen-oekraine">Oorlog in Oekraïne</a>
                             </li>
                             <li class="nav_subitem">
-                                <a href="https://www.nu.nl/binnenland">Binnenland</a>
+                                <a class="patatje" href="https://www.nu.nl/binnenland">Binnenland</a>
                             </li>
                             <li class="nav_subitem">
-                                <a href="https://www.nu.nl/buitenland">Buitenland</a>
+                                <a class="patatje" href="https://www.nu.nl/buitenland">Buitenland</a>
                             </li>
                             <li class="nav_subitem">
-                                <a href="https://www.nu.nl/algemeen">Algemeen</a>
+                                <a class="patatje" href="https://www.nu.nl/algemeen">Algemeen</a>
                             </li>
                             <li class="nav_subitem">
-                                <a href="https://www.nu.nl/politiek">Politiek</a>
+                                <a class="patatje" href="https://www.nu.nl/politiek">Politiek</a>
                             </li>
                             <li class="nav_subitem">
-                                <a href="https://www.nu.nl/uit-andere-media">Uit andere media</a>
+                                <a class="patatje" href="https://www.nu.nl/uit-andere-media">Uit andere media</a>
                             </li>
                             <li class="nav_subitem">
-                                <a href="https://www.nu.nl/videos">Video</a>
+                                <a class="patatje" href="https://www.nu.nl/videos">Video</a>
                             </li>
                             <li class="nav_subitem">
-                                <a href="https://www.nu.nl/podcast">Podcast</a>
+                                <a class="patatje" href="https://www.nu.nl/podcast">Podcast</a>
                             </li>
                             <li class="nav_subitem">
-                                <a href="https://www.nu.nl/weer">Weer</a>
+                                <a class="patatje" href="https://www.nu.nl/weer">Weer</a>
                             </li>
                         </ul>
                     </li>
                     <li class="nav_item item_nuzakelijk">
-                        <a class="economie" href="https://www.nu.nl/economie">Economie</a>
+                        <a class="economie" class="patatje" href="https://www.nu.nl/economie">Economie</a>
                         <ul class="nav_submenu">
                             <li class="nav_subitem">
-                                <a href="https://www.nu.nl/klimaat">Klimaat</a>
+                                <a class="patatje" href="https://www.nu.nl/klimaat">Klimaat</a>
                             </li>
                             <li class="nav_subitem">
-                                <a href="https://www.nu.nl/tech">Tech</a>
+                                <a class="patatje" href="https://www.nu.nl/tech">Tech</a>
                             </li>
                             <li class="nav_subitem">
-                                <a href="https://www.nu.nl/wonen">Wonen</a>
+                                <a class="patatje" href="https://www.nu.nl/wonen">Wonen</a>
                             </li>
                             <li class="nav_subitem">
-                                <a href="https://www.nu.nl/geldzaken">Geld</a>
+                                <a class="patatje" href="https://www.nu.nl/geldzaken">Geld</a>
                             </li>
                             <li class="nav_subitem">
-                                <a href="https://www.nu.nl/werk">Werk</a>
+                                <a class="patatje" href="https://www.nu.nl/werk">Werk</a>
                             </li>
                             <li class="nav_subitem">
-                                <a href="https://www.nu.nl/auto">Auto</a>
+                                <a class="patatje" href="https://www.nu.nl/auto">Auto</a>
                             </li>
                             <li class="nav_subitem">
-                                <a href="https://www.nu.nl/aandelen">Aandelen</a>
+                                <a class="patatje" href="https://www.nu.nl/aandelen">Aandelen</a>
                             </li>
                             <li class="nav_subitem">
-                                <a href="https://www.nu.nl/verkeer">Verkeer</a>
+                                <a class="patatje" href="https://www.nu.nl/verkeer">Verkeer</a>
                             </li>
                         </ul>
                     </li>
                     <li class="nav_itemitem_nusport">
-                        <a class="sport" href="https://www.nu.nl/sport">Sport</a>
+                        <a class="sport" class="patatje" href="https://www.nu.nl/sport">Sport</a>
                         <ul class="nav_submenu">
                             <li class="nav_subitem">
-                                <a href="https://www.nu.nl/voetbal">Voetbal</a>
+                                <a class="patatje" href="https://www.nu.nl/voetbal">Voetbal</a>
                             </li>
                             <li class="nav_subitem">
-                                <a href="https://www.nu.nl/formule-1">Formule 1</a>
+                                <a class="patatje" href="https://www.nu.nl/formule-1">Formule 1</a>
                             </li>
                             <li class="nav_subitem">
-                                <a href="https://www.nu.nl/live/voetbal">Scorebord</a>
+                                <a class="patatje" href="https://www.nu.nl/live/voetbal">Scorebord</a>
                             </li>
                             <li class="nav_subitem">
-                                <a href="https://www.nu.nl/sportspellen">Sportspellen</a>
+                                <a class="patatje" href="https://www.nu.nl/sportspellen">Sportspellen</a>
                             </li>
                         </ul>
                     </li>
                     <li class="nav_itemitem_nuuit">
-                        <a class="maatschapij" href="https://www.nu.nl/entertainment">Media en Cultuur</a>
+                        <a class="maatschapij" class="patatje" href="https://www.nu.nl/entertainment">Media en Cultuur</a>
                         <ul class="nav_submenu">
                             <li class="nav_subitem">
-                                <a href="https://www.nu.nl/film">Films en series</a>
+                                <a class="patatje" href="https://www.nu.nl/film">Films en series</a>
                             </li>
                             <li class="nav_subitem">
-                                <a href="https://www.nu.nl/muziek">Muziek</a>
+                                <a class="patatje" href="https://www.nu.nl/muziek">Muziek</a>
                             </li>
                             <li class="nav_subitem">
-                                <a href="https://www.nu.nl/cultuur-overig">Boek en cultuur</a>
+                                <a class="patatje" href="https://www.nu.nl/cultuur-overig">Boek en cultuur</a>
                             </li>
                             <li class="nav_subitem">
-                                <a href="https://www.nu.nl/media">Media</a>
+                                <a class="patatje" href="https://www.nu.nl/media">Media</a>
                             </li>
                             <li class="nav_subitem">
-                                <a href="https://www.nu.nl/achterklap">Achterklap</a>
+                                <a class="patatje" href="https://www.nu.nl/achterklap">Achterklap</a>
                             </li>
                             <li class="nav_subitem">
-                                <a href="https://www.nu.nl/koningshuis">Koningshuis</a>
+                                <a class="patatje" href="https://www.nu.nl/koningshuis">Koningshuis</a>
                             </li>
                             <li class="nav_subitem">
-                                <a href="https://www.nu.nl">TV gids</a>
+                                <a class="patatje" href="https://www.nu.nl">TV gids</a>
                             </li>
                         </ul>
                     </li>
                     <li class="nav_item item_nuoverig">
-                        <a class="overig" href="https://www.nu.nl/overig">Overig</a>
+                        <a class="overig" class="patatje" href="https://www.nu.nl/overig">Overig</a>
                         <ul class="nav_submenu">
                             <li class="nav_subitem">
-                                <a href="https://www.nu.nl/spellen">Het Woord</a>
+                                <a class="patatje" href="https://www.nu.nl/spellen">Het Woord</a>
                             </li>
                             <li class="nav_subitem">
-                                <a href="https://www.nu.nl/kind-gezin">Kind &amp; Gezin</a>
+                                <a class="patatje" href="https://www.nu.nl/kind-gezin">Kind &amp; Gezin</a>
                             </li>
                             <li class="nav_subitem">
-                                <a href="https://www.nu.nl/dieren">Dieren</a>
+                                <a class="patatje" href="https://www.nu.nl/dieren">Dieren</a>
                             </li>
                             <li class="nav_subitem">
-                                <a href="https://www.nu.nl/eten-en-drinken">Eten en drinken</a>
+                                <a class="patatje" href="https://www.nu.nl/eten-en-drinken">Eten en drinken</a>
                             </li>
                             <li class="nav_subitem">
-                                <a href="https://www.nu.nl/gezondheid">Gezondheid</a>
+                                <a class="patatje" href="https://www.nu.nl/gezondheid">Gezondheid</a>
                             </li>
                             <li class="nav_subitem">
-                                <a href="https://www.nu.nl/nucheckt">NUcheckt</a>
+                                <a class="patatje" href="https://www.nu.nl/nucheckt">NUcheckt</a>
                             </li>
                             <li class="nav_subitem">
-                                <a href="https://www.nu.nl/opmerkelijk">Opmerkelijk</a>
+                                <a class="patatje" href="https://www.nu.nl/opmerkelijk">Opmerkelijk</a>
                             </li>
                             <li class="nav_subitem">
-                                <a href="https://www.nu.nl/wetenschap">Wetenschap</a>
+                                <a class="patatje" href="https://www.nu.nl/wetenschap">Wetenschap</a>
                             </li>
                         </ul>
                     </li>
                 </ul>
                 <ul class="dropdown-menu-sidebar-links">
                     <li class="nav_item item_default">
-                        <a href="https://www.nu.nl/redactie.html">Contact met de redactie</a>
+                        <a class="patatje" href="https://www.nu.nl/redactie.html">Contact met de redactie</a>
                     </li>
                     <li class="nav_item item_default">
-                        <a href="https://www.nu.nl/colofon.html">Colofon</a>
+                        <a class="patatje" href="https://www.nu.nl/colofon.html">Colofon</a>
                     </li>
                     <li class="nav_item item_default">
-                        <a href="https://www.nu.nl/nieuwsbrief.html">Nieuwsbrieven</a>
+                        <a class="patatje" href="https://www.nu.nl/nieuwsbrief.html">Nieuwsbrieven</a>
                     </li>
                     <li class="nav_item ">
-                        <a href="https://www.nu.nl/over-nu">Over NU.nl</a>
+                        <a class="patatje" href="https://www.nu.nl/over-nu">Over NU.nl</a>
                     </li>
                     <li class="nav_item item_default">
-                        <a href="https://www.nu.nl/copyright.html">Copyright</a>
+                        <a class="patatje" href="https://www.nu.nl/copyright.html">Copyright</a>
                     </li>
                     <li class="nav_item item_default">
-                        <a href="https://www.nu.nl/disclaimer.html">Disclaimer</a>
+                        <a class="patatje" href="https://www.nu.nl/disclaimer.html">Disclaimer</a>
                     </li>
                     <li class="nav_item item_default">
-                        <a href="https://privacy.dpgmedia.nl/nl/">Privacy en cookiebeleid</a>
+                        <a class="patatje" href="https://privacy.dpgmedia.nl/nl/">Privacy en cookiebeleid</a>
                     </li>
                     <li class="nav_item ">
-                        <a href="javascript:window.show_consent_dialog();">Privacy instellingen</a>
+                        <a class="patatje" href="javascript:window.show_consent_dialog();">Privacy instellingen</a>
                     </li>
                     <li class="nav_item item_default">
-                        <a href="https://www.nu.nl/klachten.html">Klachten / Feedback</a>
+                        <a class="patatje" href="https://www.nu.nl/klachten.html">Klachten / Feedback</a>
                     </li>
                     <li class="nav_item item_default">
-                        <a href="https://www.nu.nl/toegankelijkheid.html">Toegankelijkheid</a>
+                        <a class="patatje" href="https://www.nu.nl/toegankelijkheid.html">Toegankelijkheid</a>
                     </li>
                     <li class="nav_item item_default">
-                        <a href="https://myprivacy.dpgmediagroup.net/consent?siteKey=tafjdbp1v2jwo1ff&callbackUrl=https%3A%2F%2Fwww.dpgmediagroup.com%2Fprivacygate-confirm%3FredirectUri%3D%252Fnl-NL%252Fnu">Adverteren</a>
+                        <a class="patatje" href="https://myprivacy.dpgmediagroup.net/consent?siteKey=tafjdbp1v2jwo1ff&callbackUrl=https%3A%2F%2Fwww.dpgmediagroup.com%2Fprivacygate-confirm%3FredirectUri%3D%252Fnl-NL%252Fnu">Adverteren</a>
                     </li>
                     <li class="nav_item ">
-                        <a href="https://www.nu.nl/mobiel.html">Apps</a>
+                        <a class="patatje" href="https://www.nu.nl/mobiel.html">Apps</a>
                     </li>
                     <li class="nav_item item_default">
-                        <a href="https://www.nu.nl/vacatures">Werken bij NU.nl</a>
+                        <a class="patatje" href="https://www.nu.nl/vacatures">Werken bij NU.nl</a>
                     </li>
                     <li class="nav_item item_default">
-                        <a href="https://www.nu.nl/independer">Verzekeringvergelijker</a>
+                        <a class="patatje" href="https://www.nu.nl/independer">Verzekeringvergelijker</a>
                     </li>
                 </ul>
             </div>
@@ -209,10 +209,10 @@
                 <div class="socials">
                     <h6>Volg ons op</h6>
                     <ul class="social-badges">
-                        <li class="instagram"><a href="https://www.instagram.com/nu.nl/" target="_blank" class="trackevent"><img class="img5" src="Instagram-Icon.png" alt=""></a></li>
-                        <li class="facebook"><a href="https://www.facebook.com/NUnl" target="_blank" class="trackevent"><img class="img5" src="Facebook_icon.svg.png" alt=""></a></li>
-                        <li class="twitter"><a href="https://www.twitter.com/NUnl" target="_blank" class="trackevent"><img class="img5" src="Twitter.png" alt=""></a></li>
-                        <li class="rss"><a href="/rss" target="_blank" class="trackevent"><img class="img5" src="3670120.png" alt=""></a></li>
+                        <li class="instagram"><a class="patatje" href="https://www.instagram.com/nu.nl/" target="_blank" class="trackevent"><img class="img" src="../footernu.nl/Instagram-Icon.png" alt="Voorpagina"> </a></li>
+                        <li class="facebook"><a class="patatje" href="https://www.facebook.com/NUnl" target="_blank" class="trackevent"><img class="img" src="../footernu.nl/Facebook_icon.svg.png" alt="Voorpagina"> </a></li>
+                        <li class="twitter"><a class="patatje" href="https://www.twitter.com/NUnl" target="_blank" class="trackevent"><img class="img" src="../footernu.nl/Logo_of_Twitter.svg.png" alt="Voorpagina"></a></li>
+                        <li class="rss"><a class="patatje" href="/rss" target="_blank" class="trackevent"><img class="img" src="../footernu.nl/3670120.png" alt="Voorpagina"></a></li>
                     </ul>
                 </div>
                 <div class="copyright">
